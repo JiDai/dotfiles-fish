84d0c6f19b675eb729b513c75b984df5b13743c2
Jordi Dosne <jordi@dosne.net>
2019-07-11 09:22:37 +0200

    Fix version function

--- symlinks/config/fish/functions/versions.fish
+++ symlinks/config/fish/functions/versions.fish
@@ -4,7 +4,7 @@ function versions --description "Show Python and Node/NPM versions number"
     end
 
     if command -v node > /dev/null 2>&1
-        echo "Node $(node --version | sed 's/v//g')"
-        echo "NPM $(npm --version)"
+        echo "Node (node --version | sed 's/v//g')"
+        echo "NPM (npm --version)"
     end
 end
