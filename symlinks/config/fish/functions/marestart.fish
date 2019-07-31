fefed2f2bf745d8b04ef4e4db50d31daa041ad3f
Jordi Dosne <jordi@dosne.net>
2019-07-31 14:20:40 +0200

    Add apps to restart scripts

--- symlinks/config/fish/functions/marestart.fish
+++ symlinks/config/fish/functions/marestart.fish
@@ -1,5 +1,5 @@
 function marestart --description "[MA] Restart all apps and services or one if specified"
-    set apps 'www' 'Portfolio' 'MediaAPI' 'MarketAPI' 'GeoAPI' 'MailAPI' 'IndiceAPI' 'MyPro' 'MyProAPI' 'Tools' 'Partners' 'ProfileAPI' 'PdfAPI' 'WebAnalytics' 'Barometre' 'ThirdPartiesAPI'
+    set apps 'www' 'Portfolio' 'MediaAPI' 'MarketAPI' 'GeoAPI' 'MailAPI' 'IndiceAPI' 'MyPro' 'MyProAPI' 'Tools' 'Partners' 'ProfileAPI' 'PdfAPI' 'WebAnalytics' 'Barometre' 'ThirdPartiesAPI' 'SalesforceAPI' 'redis'
 
     if test -n "$argv[1]"
         set apps $argv[1]
