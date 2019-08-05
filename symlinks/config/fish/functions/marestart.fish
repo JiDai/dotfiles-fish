d73908a79f804c784c66fe5fec62c1daac0db122
Jordi Dosne <jordi@dosne.net>
2019-08-05 08:36:06 +0200

    Enable restarting services

--- symlinks/config/fish/functions/marestart.fish
+++ symlinks/config/fish/functions/marestart.fish
@@ -1,8 +1,12 @@
 function marestart --description "[MA] Restart all apps and services or one if specified"
-    set apps 'www' 'Portfolio' 'MediaAPI' 'MarketAPI' 'GeoAPI' 'MailAPI' 'IndiceAPI' 'MyPro' 'MyProAPI' 'Tools' 'Partners' 'ProfileAPI' 'PdfAPI' 'WebAnalytics' 'Barometre' 'ThirdPartiesAPI' 'SalesforceAPI' 'redis'
+    set apps 'www' 'Portfolio' 'MediaAPI' 'MarketAPI' 'GeoAPI' 'MailAPI' 'IndiceAPI' 'MyPro' 'MyProAPI' 'Tools' 'Partners' 'ProfileAPI' 'PdfAPI' 'WebAnalytics' 'Barometre' 'ThirdPartiesAPI' 'SalesforceAPI'
+    set services 'nginx' 'redis'
+
+    sudo systemctl daemon-reload # If sysctl configuration has changed
 
     if test -n "$argv[1]"
-        set apps $argv[1]
+        sudo systemctl restart "$argv[1]"
+        return 0
     end
 
     for i in $apps
@@ -11,5 +15,10 @@ function marestart --description "[MA] Restart all apps and services or one if s
 
         echo "Restarting $appName..."
         sudo systemctl restart $appNameLower
+    end
+
+    for i in $services
+        echo "Restarting $i..."
+        sudo systemctl restart $i
     end
 end
