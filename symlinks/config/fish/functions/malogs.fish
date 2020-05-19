08eb3e9c88c9f54fdc2e0d6d109715582a1dce8d
Jordi Dosne <jordi@dosne.net>
2020-05-19 08:59:05 +0200

    feat: add docker apps to logging function

--- symlinks/config/fish/functions/malogs.fish
+++ symlinks/config/fish/functions/malogs.fish
@@ -1,7 +1,9 @@
 function malogs --description "[MA] Show logs of all apps or ones if specified"
     set apps 'www' 'Portfolio' 'MediaAPI' 'MarketAPI' 'GeoAPI' 'MailAPI' 'IndiceAPI' 'MyPro' 'MyProAPI' 'Tools' 'Partners' 'ProfileAPI' 'PdfAPI' 'WebAnalytics' 'Barometre' 'ThirdPartiesAPI'  'Thumbor'
     set apps_docker 'Wa'
-    set apps_docker_external 'ConnectAPI'
+    set apps_docker_external 'ConnectAPI' 'MyAccount' 'MyAccountAPI' 'UserAPI'
+
+    # TODO check status of app before display the logs
 
     if ! test -n "$argv[1]"
         echo "Please specify an app folder"
@@ -11,11 +13,9 @@ function malogs --description "[MA] Show logs of all apps or ones if specified"
     set app "$argv[1]"
     set app_name_lower (echo $app | tr '[:upper:]' '[:lower:]')
 
-    if not contains $app $apps; and not contains $app $apps_docker; and not contains $app $apps_docker_external
-        echo "Please specify a valid app folder : "
-        echo "$apps"
-        echo "$apps_docker"
-        return 1
+    if test $app = "php"
+        cat <(docker exec ma-legacy tail -n 50 -f "/var/log/syslog" & docker logs --tail 50 -f ma-legacy)
+        return 0
     end
 
     if contains $app $apps
@@ -26,19 +26,26 @@ function malogs --description "[MA] Show logs of all apps or ones if specified"
             return 1
         end
 
+        echo "Logging $app..."
         sudo tail -f $app_logs_path
         return 0
     end
 
     if contains $app $apps_docker
+        echo "Logging $app..."
         cd "$HOME/meilleursagents/apps/$app"
         docker logs --follow --tail 20 $app_name_lower"-api"
         return 0
     end
 
-    if contains $app $apps_docker_external
+    if contains "$app" $apps_docker_external
+        echo "Logging $app..."
         cd "$HOME/$app"
         docker-compose logs --follow --tail 20
         return 0
     end
+
+    echo "Please specify a valid app folder : "
+    echo "$apps" "$apps_docker" "$apps_docker_external"
+    return 1
 end
