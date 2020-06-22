400474bf7a459ba98686f045f92d1b362a82605f
Jordi Dosne <jordi@dosne.net>
2020-06-22 09:20:01 +0200

    fix: maip function argument

--- symlinks/config/fish/functions/maip.fish
+++ symlinks/config/fish/functions/maip.fish
@@ -1,7 +1,7 @@
 function maip --description "[MA] Get ip of dev server instance"
     set -l instance "jordid"
     if test -n "$argv[1]"
-        set instance "jordid"
+        set instance "$argv[1]"
     end
     gcloud --project=ma-dev2 compute instances list | grep "$instance-developers" | awk '{ print $5 }'
 end
