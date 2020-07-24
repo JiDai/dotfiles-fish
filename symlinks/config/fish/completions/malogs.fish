751ddd754d766707473dc6470146015f35c6231a
Jordi Dosne <jordi@dosne.net>
2020-07-24 10:34:22 +0200

    fix(ma): force completion to not provide file suggestion

--- symlinks/config/fish/completions/malogs.fish
+++ symlinks/config/fish/completions/malogs.fish
@@ -1,3 +1,5 @@
 for i in (seq (count $MA_APPS_SERVICES))
-  complete -c malogs -a $MA_APPS_SERVICES[$i] -f -d $MA_APPS_PATHS[$i]
+    complete -c malogs -n "__fish_is_first_token" -a $MA_APPS_SERVICES[$i] -f -d $MA_APPS_PATHS[$i]
 end
+
+complete -c malogs -f
--- symlinks/config/fish/completions/marestart.fish
+++ symlinks/config/fish/completions/marestart.fish
@@ -1,3 +1,5 @@
 for i in (seq (count $MA_APPS_SERVICES))
-    complete -c marestart -a $MA_APPS_SERVICES[$i] -f -d $MA_APPS_PATHS[$i]
+    complete -c marestart -n "__fish_is_first_token" -a $MA_APPS_SERVICES[$i] -f -d $MA_APPS_PATHS[$i]
 end
+
+complete -c marestart -f
