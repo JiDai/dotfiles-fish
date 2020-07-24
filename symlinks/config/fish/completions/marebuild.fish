e16624fd4c3dbff72c627d32256fdfe7f89b2cf5
Jordi Dosne <jordi@dosne.net>
2020-07-24 10:34:22 +0200

    fix(ma): force completion to not provide file suggestion

--- symlinks/config/fish/completions/marebuild.fish
+++ symlinks/config/fish/completions/marebuild.fish
@@ -1,3 +1,5 @@
 for i in (seq (count $MA_APPS_SERVICES))
-    complete -c marebuild -a $MA_APPS_SERVICES[$i] -f -d $MA_APPS_PATHS[$i]
+    complete -c marebuild -n "__fish_is_first_token" -a $MA_APPS_SERVICES[$i] -f -d $MA_APPS_PATHS[$i]
 end
+
+complete -c marebuild -f
