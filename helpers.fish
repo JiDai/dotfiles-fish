efb6480b420a0ab25b0f8e573ad411a76feba53e
Jordi Dosne <jordi@dosne.net>
2020-11-12 20:11:46 +0100

    feat(ma): revamp mastatus

--- helpers.fish
+++ helpers.fish
@@ -6,16 +6,29 @@ function print_title
   printf "\n$light_blue$line\n# $argv[1]\n$line$reset\n"
 end
 
-function print_subtitle 
+function print_subtitle
   set light_green '\033[1;32m'
   set reset '\033[0m' # No Color
   printf "\n$light_green## $argv[1]$reset\n"
 end
 
 
-function print_error 
+function print_error
   set red '\033[0;31m'
   set reset '\033[0m' # No Color
   printf "\n$redError: $argv[1]$reset\n"
 end
 
+
+function print_green
+  set green '\033[1;32m'
+  set reset '\033[0m' # No Color
+  printf "$green$argv[1]$reset\n"
+end
+
+
+function print_red
+  set red '\033[0;31m'
+  set reset '\033[0m' # No Color
+  printf "$red$argv[1]$reset\n"
+end
--- symlinks/config/fish/functions/malogs.fish
+++ symlinks/config/fish/functions/malogs.fish
@@ -1,25 +1,22 @@
 function malogs --description "[MA] Show logs of all apps or ones if specified"
-    set apps 'www' 'Portfolio' 'MediaAPI' 'MarketAPI' 'GeoAPI' 'MailAPI' 'IndiceAPI' 'MyPro' 'MyProAPI' 'Tools' 'Partners' 'ProfileAPI' 'PdfAPI' 'WebAnalytics' 'Barometre' 'ThirdPartiesAPI'  'Thumbor'
-    set apps_docker 'Wa'
-    set apps_docker_external 'ConnectAPI' 'MyAccount' 'MyAccountAPI' 'UserAPI'
+    set apps_docker_external 'connectapi' 'myaccount' 'myaccountapi' 'userapi' 'dvfapi' 'cookiebanner' 'webanalytics'
 
     # TODO check status of app before display the logs
 
-    if ! test -n "$argv[1]"
-        echo "Please specify an app folder"
-        return 1
-    end
-
     set app "$argv[1]"
-    set app_name_lower (echo $app | tr '[:upper:]' '[:lower:]')
 
     if test $app = "php"
         cat <(docker exec ma-legacy tail -n 50 -f "/var/log/syslog" & docker logs --tail 50 -f ma-legacy)
         return 0
     end
 
-    if contains $app $apps
-        set app_logs_path "/var/log/ma/$app_name_lower.log"
+    if contains "$app" $apps_docker_external
+        echo "Logging $app..."
+        cd "$HOME/$app"
+        docker-compose logs --follow --tail 20
+        return 0
+    else
+        set app_logs_path "/var/log/ma/$app.log"
 
         if ! test -n "$app_logs_path"
             echo "No log file for $app"
@@ -31,21 +28,7 @@ function malogs --description "[MA] Show logs of all apps or ones if specified"
         return 0
     end
 
-    if contains $app $apps_docker
-        echo "Logging $app..."
-        cd "$HOME/meilleursagents/apps/$app"
-        docker logs --follow --tail 20 $app_name_lower"-api"
-        return 0
-    end
-
-    if contains "$app" $apps_docker_external
-        echo "Logging $app..."
-        cd "$HOME/$app"
-        docker-compose logs --follow --tail 20
-        return 0
-    end
-
     echo "Please specify a valid app folder : "
-    echo "$apps" "$apps_docker" "$apps_docker_external"
+    echo "$MA_APPS_SERVICES"
     return 1
 end
