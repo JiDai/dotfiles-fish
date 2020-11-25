a2d82534b03aec1a90403237d99573900292c12a
Jordi Dosne <jordi@dosne.net>
2020-11-25 16:43:26 +0100

    fix: remove duplicate functions

--- symlinks/config/fish/functions/mapass_update_keys.fish
+++
@@ -1,4 +0,0 @@
-function mapass_update_keys -d "[MA] Update GPG keys for pass"
-    ls ~/.password-store/.public_keys/*.gpg | xargs gpg --import
-    cat ~/.password-store/.public_keys/gpg.conf >> ~/.gnupg/gpg.conf
-end
