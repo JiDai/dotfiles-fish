dbfecdf66031e1a74a53ffa960350c9fa918cc02
Jordi Dosne <jordi@dosne.net>
2021-08-06 16:05:23 +0200

    chore(nvm): update nvm version

--- install/nvm.fish
+++ install/nvm.fish
@@ -2,17 +2,17 @@
 
 function main
     set -e NVM_DIR # Avoid NVM install error if NVM_DIR is set
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
     echo "Install fish-nvm"
     omf install https://github.com/FabioAntunes/fish-nvm
 
     export NVM_SYMLINK_CURRENT=true
 
-    echo "Install and use Node 12"
-    nvm install lts/erbium
-    nvm alias default lts/erbium
-    nvm use --delete-prefix lts/erbium
+    echo "Install and use Node 14"
+    nvm install 14
+    nvm alias default 14
+    nvm use --delete-prefix 14
 
     echo "Install NPM global packages"
     npm i -g npm-check
