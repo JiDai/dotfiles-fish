4735a79c3e31ba79f54c8ed46a600547a5a0e1a1
Jordi Dosne <jordi@dosne.net>
2021-12-13 11:20:36 +0100

    fix: install fish

--- install/_omf.fish
+++ install/_omf.fish
@@ -4,6 +4,6 @@ if test -d "$HOME/opt/omf"
     rm -rf "$HOME/opt/omf"
 end
 
-curl -L https://get.oh-my.fish > install_omf
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install_omf
 fish install_omf --noninteractive --path=$HOME/opt/omf --config=./config/omf
 rm -f install_omf
