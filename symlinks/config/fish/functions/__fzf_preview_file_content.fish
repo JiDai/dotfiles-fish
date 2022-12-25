6059e773ffa0f726ca842306b855aff897aece83
Jordi Dosne <jidai@dosne.net>
2022-12-25 01:46:34 +0100

    feat: add video preview

--- symlinks/config/fish/functions/__fzf_preview_file_content.fish
+++ symlinks/config/fish/functions/__fzf_preview_file_content.fish
@@ -15,7 +15,8 @@ function __fzf_preview_file_content
                     bat $bat_args -l json $argv
                 case image/{gif,jpeg,png,svg+xml,webp}
                     timg -g (math $COLUMNS - 2)x$LINES --frames 1 $argv
-                    echo timg -g (math $COLUMNS - 2)x$LINES --frames 1 $argv
+                case video/quicktime
+                    timg -g (math $COLUMNS - 2)x$LINES --frames 1 $argv
                 case application/{msword,vnd.{ms-excel,ms-powerpoint,openxmlformats-officedocument.{presentationml.presentation,spreadsheetml.sheet,wordprocessingml.document}},pdf} image/{heic,x-icns} video/{mp4,x-matroska}
                     set tmp (mktemp -d)
                     qlmanage -t -s (math $COLUMNS x 8) -o $tmp $argv &>/dev/null
