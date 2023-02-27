64d6e9f506ab3b45d16b1d303548814ef597b795
Jordi Dosne <jordi.dosne@spacefill.fr>
2023-02-27 15:22:46 +0100

    fix(spacefill): db script wa not working

--- symlinks/config/fish/functions/sfrestoredbdate.fish
+++ symlinks/config/fish/functions/sfrestoredbdate.fish
@@ -1,19 +1,17 @@
-function sfrestoredbdate --description "[SF] Restore the DB at a date"
+function sfrestoredbdatetolocal --description "[SF] Restore the DB"
 	if ! docker info > /dev/null 2>&1
 		echo "Docker is not running"
 		return 1
 	end
 
-	set -l date_string "$1"
-	if test -z "$date_string" ]
-		set -l date_string "(date +%Y%m%d)"
-	else
-		set -l date_string "$1"
+	set -l backup_name "$argv[1]"
+	if test -z "$backup_name"
+		set backup_name (date +%Y%m%d)
 	end
 
-	echo "Restoring db for $date_string..."
+	echo "Restoring db for $backup_name..."
 	import-postgres-backup-snapshot.sh
 	replace-all-password-locally.sh
-	dump-local-db.sh "$date_string"
-	import-db-locally.sh "$date_string"
+	dump-local-db.sh "$backup_name"
+	import-db-locally.sh "$backup_name"
 end
