71047c38036b76dfe75420330d0f9a2ae7a884ea
Jordi Dosne <jordi.dosne@spacefill.fr>
2023-03-06 17:50:28 +0100

    feat(spacefill): new browser e2e test shortcut

---
+++ symlinks/config/fish/functions/sfe2ebrowsertest.fish
@@ -0,0 +1,9 @@
+function sfe2ebrowsertest --description "[SF] Run a e2e test in chromium without closing"
+	set -l tests "$argv[1]"
+	if test -z "$tests"
+		echo "Please specify a test"
+		return 1
+	end
+
+	FRONTENDS_E2E_DISABLE_CLOSE_BROWSER=1 yarn run test:e2e-disable-headless $tests
+end
