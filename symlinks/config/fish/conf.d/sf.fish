3a0fe3c8198eee2db66ef957b669a05990bc9935
Jordi Dosne <jordi.dosne@spacefill.fr>
2023-03-20 10:17:06 +0100

    feat(spacefill): shortcut for generating i18n files

--- symlinks/config/fish/conf.d/sf.fish
+++ symlinks/config/fish/conf.d/sf.fish
@@ -9,5 +9,6 @@ abbr sffa "cd $SF_PATH/services/frontends/packages/admin-console/"
 abbr sffe "cd $SF_PATH/services/frontends/packages/external-console/"
 abbr sfe2e "cd $SF_PATH/services/frontends/packages/end2end"
 abbr sfgqlupdate "npm --prefix $SF_PATH/services/frontends/packages/shared run download-graphql-schema"
+abbr sfi18n "npm --prefix $SF_PATH/services/frontends/packages/shared run i18n"
 
 set -gx PATH "$SF_PATH/scripts" "$PATH"
