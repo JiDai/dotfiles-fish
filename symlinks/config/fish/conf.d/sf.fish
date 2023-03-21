5c7824cd82048f42c9477acef7a582dab3a6ccfb
Jordi Dosne <jordi.dosne@spacefill.fr>
2023-03-21 16:22:07 +0100

    feat(spacefill): shortcut for DB

--- symlinks/config/fish/conf.d/sf.fish
+++ symlinks/config/fish/conf.d/sf.fish
@@ -5,6 +5,7 @@ set -gx DIRENV_LOG_FORMAT
 abbr wk "cd ~/Work/"
 abbr sf "cd $SF_PATH"
 abbr sff "cd $SF_PATH/services/frontends"
+abbr sfd "cd $SF_PATH/databases"
 abbr sffa "cd $SF_PATH/services/frontends/packages/admin-console/"
 abbr sffe "cd $SF_PATH/services/frontends/packages/external-console/"
 abbr sfe2e "cd $SF_PATH/services/frontends/packages/end2end"
