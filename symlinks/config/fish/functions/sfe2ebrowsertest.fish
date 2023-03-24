fc94bde1a9d5332d765d9856270ecb99eedbd7a9
Jordi Dosne <jordi.dosne@spacefill.fr>
2023-03-24 12:23:55 +0100

    fix(spacefill): argument to e2e command

--- symlinks/config/fish/functions/sfe2ebrowsertest.fish
+++ symlinks/config/fish/functions/sfe2ebrowsertest.fish
@@ -5,5 +5,5 @@ function sfe2ebrowsertest --description "[SF] Run a e2e test in chromium without
 		return 1
 	end
 
-	FRONTENDS_E2E_DISABLE_CLOSE_BROWSER=1 yarn run test:e2e-disable-headless $tests
+	FRONTENDS_E2E_DISABLE_CLOSE_BROWSER=1 yarn run test:e2e-disable-headless $argv
 end
