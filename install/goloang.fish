280d82a5fa00ee0957f0931c031554dca408a37e
Jordi Dosne <f_github@jidai.fr>
2023-04-17 22:06:34 +0200

    fix: handle go reinstall for updates

--- install/goloang.fish
+++ install/goloang.fish
@@ -1,23 +1,21 @@
 function main
-  set GO_VERSION "1.19"
+  set GO_VERSION "1.20.2"
 
   set OS "darwin"
   set ARCH "arm64"
 
   source './symlinks/config/fish/conf.d/golang.fish'
 
-  if ! test -d "$HOME/opt/go"
-	set GO_ARCHIVE "go$GO_VERSION.$OS-$ARCH.tar.gz"
+  set GO_ARCHIVE "go$GO_VERSION.$OS-$ARCH.tar.gz"
 
-	curl -O "https://dl.google.com/go/$GO_ARCHIVE"
-	tar -C "$HOME/opt" -xzf "$GO_ARCHIVE"
-	rm -rf "$GO_ARCHIVE"
-  end
+  curl -O "https://dl.google.com/go/$GO_ARCHIVE"
+  tar -C "$HOME/opt" -xzf "$GO_ARCHIVE"
+  rm -rf "$GO_ARCHIVE"
 
   if command -v go > /dev/null 2>&1
-	go install github.com/kisielk/errcheck@latest
-	go install golang.org/x/lint/golint@latest
-	go install golang.org/x/tools/cmd/goimports@latest
+  go install github.com/kisielk/errcheck@latest
+  go install golang.org/x/lint/golint@latest
+  go install golang.org/x/tools/cmd/goimports@latest
   end
 
 end
