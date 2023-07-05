dada3ffcf7d52e010cc56c9aa74d09fa7617421b
Jordi Dosne <jordi.dosne@spacefill.fr>
2023-07-05 14:26:00 +0200

    chore: new package shortcut

--- symlinks/config/fish/conf.d/sf.fish
+++ symlinks/config/fish/conf.d/sf.fish
@@ -9,6 +9,7 @@ abbr sfd "cd $SF_PATH/databases"
 abbr sffa "cd $SF_PATH/services/frontends/packages/admin-console/"
 abbr sffe "cd $SF_PATH/services/frontends/packages/external-console/"
 abbr sfe2e "cd $SF_PATH/services/frontends/packages/end2end"
+abbr sfui "cd $SF_PATH/services/frontends/packages/uikit"
 abbr sfgqlupdate "npm --prefix $SF_PATH/services/frontends/packages/shared run download-graphql-schema"
 abbr sfi18n "npm --prefix $SF_PATH/services/frontends/packages/shared run i18n"
 
