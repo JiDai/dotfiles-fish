a7ed73d15d36e716247619a039de3f39b01b7d65
Jordi Dosne <jordi.dosne@spacefill.fr>
2024-05-23 18:14:15 +0200

    fix: reset command not working at all

--- symlinks/config/fish/functions/sfdbreset.fish
+++ symlinks/config/fish/functions/sfdbreset.fish
@@ -1,7 +1,7 @@
 function sfdbreset --description "[SF] Restore the DB prod in localhost"
 	wait_for_docker
 
-	sfd
-	./scripts/dump-managed-db-new.sh postgres://$AWS_PROD_DB_USERNAME:$AWS_PROD_DB_PASSWORD@production-replica.cwlralkfkokc.eu-west-3.rds.amazonaws.com:5432/production
+	pushd ~/Work/database
 	./scripts/reset-local-database.sh
+	popd
 end
