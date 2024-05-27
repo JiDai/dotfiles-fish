3653edd95c46ab96f97f7564c4c7b64a9ae4b2b7
Jordi Dosne <jordi.dosne@spacefill.fr>
2024-05-27 15:52:30 +0200

    fix: reset command working directory

--- symlinks/config/fish/functions/sfdbprodtolocal.fish
+++ symlinks/config/fish/functions/sfdbprodtolocal.fish
@@ -1,7 +1,8 @@
 function sfdbprodtolocal --description "[SF] Restore the DB prod in localhost"
 	wait_for_docker
 
-	sfd
+	pushd ~/Work/database
 	./scripts/dump-managed-db-new.sh postgres://$AWS_PROD_DB_USERNAME:$AWS_PROD_DB_PASSWORD@production-replica.cwlralkfkokc.eu-west-3.rds.amazonaws.com:5432/production
 	./scripts/import-db-locally.sh dump_db
+	popd
 end
